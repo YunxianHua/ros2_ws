d07071abc933da0f75e6f8b195c37ca3c8394015
fei.gao <104708586+YunxianHua@users.noreply.github.com>
2023-11-30 14:56:16 +0800

    fix: add joint state publish thread (#3)

--- src/franka_demo/src/franka_demo_node.cpp
+++ src/franka_demo/src/franka_demo_node.cpp
@@ -4,9 +4,9 @@
 #include "rclcpp/rclcpp.hpp"
 #include "franka_msgs/srv/set_joint_stiffness.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
-#include <iostream>
 #include <thread>
 #include <memory>
+#include <atomic>
 
 struct JointSafetyRange {
     double max_value;
@@ -16,8 +16,10 @@ struct JointSafetyRange {
 };
 
 static std::array<double, 7> current_joint_state{0, 0, 0, 0, 0, 0, 0};
+static std::array<double, 7> dest_joint_state{0, 0, 0, 0, 0, 0, 0};
 
-const static int move_step_count = 20;
+const static int move_step_count(20);
+static std::atomic<bool> is_moving(false);
 
 static JointSafetyRange joint_safety_range[7]{
         JointSafetyRange(-2.90, 2.90),
@@ -29,63 +31,66 @@ static JointSafetyRange joint_safety_range[7]{
         JointSafetyRange(-2.90, 2.90)
 };
 
-void moveJointThread(const rclcpp::Logger &logger, std::array<double, 7> stiffness,
-                     rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr publisher) {
-    std::array<double, 7> move_step{};
-
+void publishJointStateThread(const std::shared_ptr<rclcpp::Node> &node) {
     rclcpp::Clock clock;
-
+    rclcpp::QoS qos_profile(10);
+    qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+    auto joint_states_publisher = node->create_publisher<sensor_msgs::msg::JointState>("/joint_states", qos_profile);
+    sensor_msgs::msg::JointState joint_states;
     for (int i = 0; i < 7; ++i) {
-        auto max = joint_safety_range[i].max_value, min = joint_safety_range[i].min_value;
-        auto dest_state = std::min(stiffness[i], max);
-        dest_state = std::max(dest_state, min);
-        move_step[i] = dest_state - current_joint_state[i];
-        current_joint_state[i] = dest_state;
+        joint_states.name.push_back("panda_joint" + std::to_string(i + 1));
+        joint_states.position.push_back(0);
     }
-    RCLCPP_INFO(logger, "move_step: [%lf, %lf, %lf, %lf, %lf, %lf, %lf,]", move_step[0],
-                move_step[1], move_step[2], move_step[3], move_step[4],
-                move_step[5], move_step[6]);
 
-    for (int i = 0; i < move_step_count; ++i) {
-        sensor_msgs::msg::JointState joint_states;
-        joint_states.header.stamp = clock.now();
+    while (rclcpp::ok()){
+        if (is_moving) {
+            std::array<double, 7> move_step{};
+            for (int i = 0; i < 7; ++i) {
+                auto max = joint_safety_range[i].max_value, min = joint_safety_range[i].min_value;
+                auto dest_state = std::min(dest_joint_state[i], max);
+                dest_state = std::max(dest_state, min);
+                move_step[i] = (dest_state - current_joint_state[i]) / move_step_count;
+            }
 
-        for (int j = 0; j < 7; ++j) {
-            joint_states.name.push_back("panda_joint" + std::to_string(j + 1));
-            auto current_state =
-                    current_joint_state[j] - move_step[j] / move_step_count * (move_step_count - 1 - i);
-            joint_states.position.push_back(current_state);
+            for (int i = 0; i < move_step_count; ++i) {
+                joint_states.header.stamp = clock.now();
+
+                joint_states.position.clear();
+                for (int j = 0; j < 7; ++j) {
+                    auto current_state = current_joint_state[j] + move_step[j];
+                    joint_states.position.push_back(current_state);
+                    current_joint_state[j] = current_state;
+                }
+                joint_states_publisher->publish(joint_states);
+                rclcpp::sleep_for(std::chrono::milliseconds(100));
+            }
+            is_moving.store(false);
+        } else {
+            joint_states.header.stamp = clock.now();
+            joint_states_publisher->publish(joint_states);
+            rclcpp::sleep_for(std::chrono::milliseconds(1000));
         }
-        publisher->publish(joint_states);
-        rclcpp::sleep_for(std::chrono::milliseconds(100));
     }
-    RCLCPP_INFO(logger, "current joint state: [%lf, %lf, %lf, %lf, %lf, %lf, %lf,]", current_joint_state[0],
-                current_joint_state[1], current_joint_state[2], current_joint_state[3], current_joint_state[4],
-                current_joint_state[5], current_joint_state[6]);
 }
-
 
 class FrankaDemoNode : public rclcpp::Node {
 public:
     FrankaDemoNode() : Node("franka_demo_node") {
-        rclcpp::QoS qos_profile(10);
-        qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
-
-        joint_states_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("/joint_states", qos_profile);
 
         service_ = create_service<franka_msgs::srv::SetJointStiffness>(
                 "set_joint_stiffness",
                 [this](const std::shared_ptr<franka_msgs::srv::SetJointStiffness::Request> request,
                        std::shared_ptr<franka_msgs::srv::SetJointStiffness::Response> response) {
+                    RCLCPP_INFO(this->get_logger(), "set_joint_stiffness service was called.");
+
                     if (request->joint_stiffness.size() != 7) {
                         response->success = false;
                         response->error = "joint stiffness size must be 7!";
                         return;
                     }
+                    is_moving.store(true);
+                    dest_joint_state = request->joint_stiffness;
 
-                    std::thread t1(moveJointThread, this->get_logger(), request->joint_stiffness,
-                                   joint_states_publisher_);
-                    t1.detach();
                     response->success = true;
                     response->error = "success!";
                 });
@@ -93,12 +98,14 @@ public:
 
 private:
     rclcpp::Service<franka_msgs::srv::SetJointStiffness>::SharedPtr service_;
-    rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_states_publisher_;
 };
 
 int main(int argc, char **argv) {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<FrankaDemoNode>();
+
+    std::thread joint_state_publisher(publishJointStateThread, node);
+    joint_state_publisher.detach();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
